e482050f92b2569bb71595947caecfbd365f39fb
pheonixeye <drkazgm@gmail.com>
2024-01-11 23:55:18 +0200

    working local function

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io' show Platform;
 import 'package:dart_appwrite/dart_appwrite.dart';
 import 'package:starter_template/models/article_meta/article_meta.dart';
@@ -16,32 +17,40 @@ Future<dynamic> main(final context) async {
 
   if (context.req.method != 'POST') {
     context.error('bad request.');
-    context.res.json({
+    return context.res.json({
       'type': 'error',
       'code': 1,
       'reason': 'bad request.',
     });
-    return;
   }
   final client = Client()
-      .setEndpoint('http://localhost:4567/v1')
+      .setEndpoint('http://192.168.0.107:4567/v1')
       .setProject(Platform.environment['APPWRITE_FUNCTION_PROJECT_ID'])
-      .setKey(Platform.environment['APPWRITE_API_KEY']);
+      .setKey(Platform.environment['APPWRITE_API_KEY'])
+      .setSelfSigned();
 
   final Databases db = Databases(client);
 
-  final body = context.req.body;
+  final body = jsonDecode(context.req.bodyRaw);
 
-  final docId = body['docid'] as String;
+  // final docId = body['docid'] as String;
+  final syndId = body['syndid'] as int;
+
+  context.log(syndId);
 
   try {
     //check if doctor with recieved id is created
-    final doctor = await db.getDocument(
-      databaseId: Platform.environment['DOCTORS_DATABASE']!,
-      collectionId:
-          Platform.environment['DOCTORS_DATABASE_DOCTORS_COLLECTION']!,
-      documentId: docId,
-    );
+    final _doctors = await db.listDocuments(
+        databaseId: Platform.environment['DATABASE_DOCTORS']!,
+        collectionId:
+            Platform.environment['DOCTORS_DATABASE_DOCTORS_COLLECTION']!,
+        queries: [Query.equal('synd_id', syndId)]);
+
+    final doctor = _doctors.documents.first;
+
+    final docId = doctor.$id;
+
+    context.log(docId);
 
     //create empty doctor-document ref in doctor-documents collection
     await db.createDocument(
@@ -143,14 +152,14 @@ Future<dynamic> main(final context) async {
     });
 
     context.log('doctor creation algorithm complete.');
-    context.res.json({
+    return context.res.json({
       'type': 'info',
       'code': 0,
       'reason': 'doctor creation algorithm complete.'
     });
   } catch (e) {
-    context.error('doctor with requested Id does not exist.');
-    context.res.json({
+    context.error(e.toString());
+    return context.res.json({
       'type': 'error',
       'code': 2,
       'reason': 'doctor with requested Id does not exist.'
